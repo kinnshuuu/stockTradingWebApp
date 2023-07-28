53e3608f269bdfe4ad6d01c39269e122aac2f09c
Kinshu Jain <kinshujain0@gmail.com>
2023-07-29 00:33:12 +0530

    login/signup page frontend

---
+++ frontend/src/components/Logout.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Logout = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // Perform logout actions (clear tokens, etc.)
+    // Redirect to the login page after logout
+    navigate('/dashboard');
+  };
+
+  return (
+    <div>
+      <h2>Logout</h2>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
+};
+
+export default Logout;
---
+++ frontend/src/components/Signup.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Signup = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('<your_api_endpoint>/api/signup', formData);
+      console.log(response.data);
+      // Handle successful signup, redirect to a new page, etc.
+    } catch (error) {
+      console.error(error.response.data);
+      // Handle error, show error message, etc.
+    }
+  };
+
+  return (
+    <div>
+      <h2>Signup</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name:</label>
+          <input type="text" name="name" value={formData.name} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input type="email" name="email" value={formData.email} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" name="password" value={formData.password} onChange={handleChange} />
+        </div>
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
